2c7cd483832c7dbf86352fd85e4b4cb34c185bd1
datchina03 <66707696+datchina03@users.noreply.github.com>
2025-11-22 12:36:37 +0530

    initial commit

---
+++ src/test/java/com/hcl/ewallet/walletapi/WalletapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.ewallet.walletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
